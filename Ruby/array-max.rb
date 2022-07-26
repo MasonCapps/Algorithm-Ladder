ee66ff5538810a99a011d9a4b961534bba183cc5
Mason Capps <masoncapps42119@gmail.com>
2022-07-26 14:06:12 -0500

    Adds solution

---
+++ Ruby/array-max.rb
@@ -0,0 +1,11 @@
+def array_max(array)
+  highest_number = array[0]
+  array.each do |x|
+    if x > highest_number
+      highest_number = x
+    end
+  end
+  highest_number
+end
+
+p array_max([5, 17, -4, 20, 12])
