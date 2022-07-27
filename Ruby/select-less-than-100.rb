18f10f3d2f68df0dd7e82050ac311f2a2dcbabeb
Mason Capps <masoncapps42119@gmail.com>
2022-07-26 23:57:41 -0500

    Adds solution

--- Ruby/select-less-than-100.rb
+++ Ruby/select-less-than-100.rb
@@ -1,5 +1,5 @@
-def less_than(array)
+def less_than_one_hundred(array)
   return array.select { |number| number < 100 }
 end
 
-p less_than([99, 101, 88, 4, 2000, 50])
+p less_than_one_hundred([99, 101, 88, 4, 2000, 50])
