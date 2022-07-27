cf668db31e7d47ac0f0f4f6f9e4471c9c21a4e2d
Mason Capps <masoncapps42119@gmail.com>
2022-07-27 16:25:23 -0500

    Adds solution

---
+++ Ruby/reverse-string.rb
@@ -0,0 +1,11 @@
+def string_reverser(string)
+  length = string.length - 1
+  index = 0
+  while index < length / 2
+    string[index], string[length - index] = string[length - index], string[index]
+    index += 1
+  end
+  return string
+end
+
+p string_reverser("abcde")
