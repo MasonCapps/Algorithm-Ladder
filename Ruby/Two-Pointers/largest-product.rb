7f57a21ed613ec079cd9feb69ac39a318942bf0e
Mason Capps <masoncapps42119@gmail.com>
2022-08-04 10:52:38 -0500

    Adds solution

--- Ruby/Two-Pointers/largest-product.rb
+++ Ruby/Two-Pointers/largest-product.rb
@@ -1,1 +1,17 @@
+def largest_product(array)
+  current_largest_product = 0
+  index = 0
+  while index < array.length
+    inner_index = index + 1
+    while inner_index < array.length
+      if array[index] * array[inner_index] > current_largest_product
+        current_largest_product = array[index] * array[inner_index]
+      end
+      inner_index += 1
+    end
+    index += 1
+  end
+  return current_largest_product
+end
 
+p largest_product([5, -2, 1, -9, -7, 2, 6])
